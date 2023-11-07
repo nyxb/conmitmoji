8dabdbf2eccb448bad13c1f1600ab26fffe9a87e
nyxb <contact@nyxb.email>
2023-11-07 12:00:53 +0100

    ♻️ refactor(prompts.ts): simplify RULES constant and convert it to EMOJIS object for better readability and maintainability 🎨 style(prompts.ts): rephrase INIT_MAIN_PROMPT function to improve clarity and readability of the instructions

--- src/prompts.ts
+++ src/prompts.ts
@@ -13,65 +13,29 @@ const config = getConfig()
 const translation = i18n[(config?.MOJI_LANGUAGE as I18nLocals) || 'en']
 
 export const IDENTITY
-  = 'You are to act as the author of a commit message in git. Follow the Conventional Emoji Commits specification: start each commit with an emoji and a type (e.g., 🩹 fix: or ✨ feat:), optionally followed by a scope. Use present tense, keep lines under 74 characters. Include BREAKING CHANGE if necessary.'
+  = 'You are to act as the author of a commit message in git.'
 
-export const RULES = `
-  - Commits MUST start with an emoji and a type, optionally followed by a scope: <emoji><type>(<scope>): <description>
-  - ONLY the following emojis and their corresponding scopes are allowed:
-    🩹 :adhesive_bandage: (fix) - Corrections of bugs or issues.
-    🤖 :robot: (ci) - Changes to CI configuration files or scripts.
-    🛠️ :hammer_and_wrench: (build) - Changes that affect the build system or external dependencies.
-    🧹 :broom: (chore) - Routine tasks or minor maintenance activities.
-    📚 :books: (docs) - Changes or additions to documentation.
-    ✨ :sparkles: (feat) - Introduction of a new feature or capability.
-    ⚡️ :rocket: (perf) - Optimizations that improve system performance.
-    ♻️ :recycle: (refactor) - Code revisions without changing functionality.
-    ⏪ :rewind: (revert) - Reverting a previous commit.
-    🎨 :art: (style) - Code style changes that don't affect functionality.
-    🧪 :test_tube: (test) - Changes related to tests.
-    🎭 :performing_arts: (animations) - Changes related to animation functionalities or effects.
-    🌍 :earth_africa: (common) - General changes or updates that don't fit into other specific categories.
-    🔍 :mag: (compiler) - Modifications related to code compilation or transformation processes.
-    🔧 :wrench: (compiler-cli) - Changes to any command-line interface related to compilation.
-    💙 :blue_heart: (core) - Fundamental changes or updates that affect the core functionalities.
-    🧩 :jigsaw: (elements) - Updates related to individual components or elements.
-    📝 :memo: (forms) - Changes related to form handling, input validation, or user input.
-    📡 :satellite: (http) - Modifications related to HTTP requests, API calls, or data fetching.
-    🗣️ :speaking_head: (language-service) - Updates to language services or translation functionalities.
-    🌐 :globe_with_meridians: (platform-browser) - Changes related to browser-specific functionalities or features.
-    🌌 :milky_way: (platform-browser-dynamic) - Dynamic implementations or updates for browser-specific functionalities.
-    🖥️ :desktop_computer: (platform-server) - Changes related to server-side functionalities or back-end changes.
-    🕸️ :spider_web: (platform-webworker) - Modifications related to web workers or background processing.
-    🕷️ :spider: (platform-webworker-dynamic) - Dynamic implementations or updates for web worker functionalities.
-    🛣️ :motorway: (router) - Updates related to routing, navigation, or URL management.
-    📊 :bar_chart: (service-worker) - Changes related to service workers or offline capabilities.
-    ⬆️ :arrow_up: (upgrade) - Modifications related to version upgrades or dependency updates.
-    📦 :package: (packaging) - Changes that affect packaging, bundling, or distribution.
-    📜 :scroll: (changelog) - Updates to logs, documentation, or annotations.
-    📘 :blue_book: (aio) - Changes related to documentation or informational content.
-  - Use present tense, keep lines under 74 characters.
-  - Include BREAKING CHANGE with 🚨 or ❗ when necessary.
-  - The description MUST immediately follow the colon and space after the type/scope prefix.
-  - A longer commit body MAY be provided after the short description, separated by a blank line.
-  - Footers follow a convention similar to git trailer format and MAY be included after the body, separated by a blank line.
-  - BREAKING CHANGE MUST be uppercase and indicated in the type/scope prefix of a commit or as a footer.
-  - Types other than ✨feat and 🩹fix MAY be used.
-  - Commits MUST NOT be treated as case sensitive, except for 🚨 BREAKING CHANGE.
-  `
+export const EMOJIS
+  = { 'fix': '🩹', 'ci': '🤖', 'build': '🛠️', 'chore': '🧹', 'docs': '📚', 'feat': '✨', 'perf': '⚡️', 'refactor': '♻️', 'revert': '⏪', 'style': '🎨', 'test': '🧪', 'animations': '🎭', 'common': '🌍', 'compiler': '🔍', 'compiler-cli': '🔧', 'core': '💙', 'elements': '🧩', 'forms': '📝', 'http': '📡', 'language-service': '🗣️', 'platform-browser': '🌐', 'platform-browser-dynamic': '🌌', 'platform-server': '🖥️', 'platform-webworker': '🕸️', 'platform-webworker-dynamic': '🕷️', 'router': '🛣️', 'service-worker': '📊', 'upgrade': '⬆️', 'packaging': '📦', 'changelog': '📜', 'aio': '📘' }
 
 function INIT_MAIN_PROMPT(language: string): ChatCompletionRequestMessage {
+   // Erstellen Sie einen String, der die Emoji-Typ-Zuordnung darstellt
+   const emojiList = Object.entries(EMOJIS)
+      .map(([type, emoji]) => `${emoji} ${type}: `)
+      .join('\n')
+
    return {
       role: ChatCompletionRequestMessageRoleEnum.System,
-      content: `${IDENTITY} 
-      Please adhere to the following rules when creating commit messages:
-      ${RULES}
-      Your mission is to create clean and comprehensive commit messages as per the conventional emoji commits convention and explain WHAT were the changes and mainly WHY the changes were done. I'll send you an output of 'git diff --staged' command, and you are to convert it into a commit message.
-    ${
-      config?.MOJI_DESCRIPTION
-        ? 'Add a short description of WHY the changes are done after the commit message. Don\'t start it with "This commit", just describe the changes.'
-        : 'Don\'t add any descriptions to the commit, only commit message.'
-    }
-    Use the present tense. Lines must not be longer than 74 characters. Use ${language} for the commit message.`,
+      content: `${IDENTITY} Your mission is to create clean and comprehensive commit messages as per the conventional commit convention. Each commit message should start with an emoji that corresponds to the change type, followed by the change type and a description. Here is the list of emojis for each change type:
+     \n${emojiList}\n
+     Based on the 'git diff --staged' output provided, convert it into a commit message that adheres to the following structure:
+     <emoji> <type>(<scope>): <description>
+     ${
+       config?.MOJI_DESCRIPTION
+         ? 'Include a short description of WHY the changes were done after the commit message.'
+         : 'Don\'t add any descriptions to the commit, only commit message.'
+     }
+     Use the present tense. Lines must not be longer than 74 characters. Use ${language} for the commit message.`,
    }
 }
 
